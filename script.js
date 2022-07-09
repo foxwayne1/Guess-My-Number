6deb841b2dd2988213246138e9446614ab592f90
foxwayne1 <fox.wayne1@gmail.com>
2022-07-09 11:24:01 +0100

    all files

---
+++ script.js
@@ -0,0 +1,56 @@
+'use strict';
+
+// secret number onload
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+
+document.querySelector('.check').addEventListener('click', function () {
+  let guess = Number(document.querySelector('.guess').value);
+
+  if (!guess) {
+    // 0 is a falsy value
+    document.querySelector('.message').textContent = '⛔ No Number';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉 Correct Number';
+    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('body').style.backgroundColor = '#60b337';
+    document.querySelector('.number').style.width = '30rem';
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈 Too High';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent =
+        '💥 You lost the game, Try again';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📉 Too Low';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent =
+        '💥 You lost the game, Try again';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  document.querySelector('.score').textContent = 20;
+  console.log(score);
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  let guess = (document.querySelector('.guess').value = '');
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.message').textContent = 'Start Guessing...';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+});
